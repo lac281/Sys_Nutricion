512fc7ab4179da57702e7f07205cbf5eec29bae5
cordon <cordon@DELL-Inspiron>
2015-10-15 16:14:12 -0600

    Clase Conexion

---
+++ App_SportSupply/src/java/App_SportSupply/model/Conexion.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package App_SportSupply.model;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author cordon
+ */
+public class Conexion {
+
+    public static final String USERNAME = "agroprio";
+    public static final String PASSWORD = "agr0p3";
+    public static final String HOST = "localhost";
+    public static final String PORT = "3306";
+    public static final String DATABASE = "agrodb";
+    public static final String CLASSNAME = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://"+HOST+":"+PORT+"/"+DATABASE;
+
+    public java.sql.Connection con;
+
+    public Conexion(){
+
+        try
+        {
+            Class.forName(CLASSNAME);
+            con = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+        } 
+        catch(ClassNotFoundException | SQLException e)
+        {
+            System.out.println("Error" + e);
+        }
+    }//Fin del constructor
+}
